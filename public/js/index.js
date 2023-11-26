cace37db0dd1721532c027aa3539d29459f5776f
Fedor Burhonov <fedor.burgonov@mdfin.com>
2023-11-26 17:29:50 +0200

    Work copy

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,28 @@
 function ready() {
+  const form = document.getElementById('form');
+  const input = document.getElementById('input');
   const container = document.getElementById('container');
   const data = 'Hello World!!!';
 
-  if (stringCheck(data)) {
-    container.innerHTML = reverseString(data);
+  function updateText() {
+    if (stringCheck(input.value)) {
+      container.innerHTML = reverseString(input.value);
+    }
   }
+
+  function onFocusHandler() {
+    this.value = '';
+
+    updateText();
+  }
+
+  form.addEventListener('submit', (event) => event.preventDefault());
+  input.addEventListener('focus', onFocusHandler);
+  input.addEventListener('input', updateText);
+
+  input.value = data;
+
+  updateText();
 }
 
 document.addEventListener('DOMContentLoaded', ready);
